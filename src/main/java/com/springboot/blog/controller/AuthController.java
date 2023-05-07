4f1cd5aebac99a255d59654816ce6f0dd6cce837
Sushil <sjsushil09@gmail.com>
2023-05-08 01:22:30 +0530

    Regsiter User functionality.

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,11 @@ public class AuthController {
     @PostMapping(value = {"/login","/signin"})
     public ResponseEntity<String> login (@RequestBody LoginDto loginDto) {
         return new ResponseEntity<>(authService.login(loginDto), HttpStatus.OK);
+    }
+
+    @PostMapping(value = {"/register","/signup"})
+    public ResponseEntity<String> login (@RequestBody RegisterDto registerDto) {
+        return new ResponseEntity<>(authService.registerUser(registerDto), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -1,22 +1,42 @@
 package com.springboot.blog.service.impl;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.entity.User;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
+import com.springboot.blog.repository.RoleRepository;
+import com.springboot.blog.repository.UserRepository;
 import com.springboot.blog.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Slf4j
 public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private static PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -29,5 +49,34 @@ public class AuthServiceImpl implements AuthService {
         log.info("Security context: {}",SecurityContextHolder.getContext());
         return "Logged-in successfully";
 
+    }
+
+    @Override
+    public String registerUser(RegisterDto registerDto) {
+        if(userRepository.existsByUserName(registerDto.getUserName()))
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"UserName already exists");
+
+        if(userRepository.existsByEmail(registerDto.getEmail()))
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"User with given email already exists");
+
+        User user = new User();
+        mapUserDtoToUser(registerDto, user);
+
+        Role role = roleRepository.findByName("ROLE_USER").get();
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        user.setRoles(roles);
+//        roleRepository.save(role);
+        userRepository.save(user);
+
+        return "User Registered Successfully !!";
+    }
+
+    private static void mapUserDtoToUser(RegisterDto registerDto, User user) {
+        user.setName(registerDto.getName());
+        user.setUserName(registerDto.getUserName());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
     }
 }
