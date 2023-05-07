da0b3ec01ca33d3d20ae30f6029ab4374c0fd2b7
Sushil <sjsushil09@gmail.com>
2023-05-07 23:57:23 +0530

    DaoBased AuthN mechanism.

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -4,26 +4,37 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Slf4j
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     //For encoding the password with BCryp. algo.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 
@@ -40,25 +51,25 @@ public class SecurityConfig {
     }
 
     //For In-memory AuthN with UserName/Password
-    @Bean
-    public UserDetailsService users() {
-        // The builder will ensure the passwords are encoded before saving in memory
-//        User.UserBuilder users = User
-        UserDetails user = User.builder()
-                .username("sushil")
-                .password(passwordEncoder().encode("sushil"))
-                .roles("USER")
-                .build();
-
-        log.info("Password for user {} ", user.getPassword());
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
-
-        log.info("Password for admin {} ", admin.getPassword());
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Bean
+//    public UserDetailsService users() {
+//        // The builder will ensure the passwords are encoded before saving in memory
+////        User.UserBuilder users = User
+//        UserDetails user = User.builder()
+//                .username("sushil")
+//                .password(passwordEncoder().encode("sushil"))
+//                .roles("USER")
+//                .build();
+//
+//        log.info("Password for user {} ", user.getPassword());
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("USER", "ADMIN")
+//                .build();
+//
+//        log.info("Password for admin {} ", admin.getPassword());
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
 }
---
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -0,0 +1,39 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String userName;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
